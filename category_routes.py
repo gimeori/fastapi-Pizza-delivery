20c78b9c57588baaaceb767404238d95893592f7
SEA <elizavetka07@gmail.com>
2024-05-19 19:20:14 +0700

    category filtering

--- category_routes.py
+++ category_routes.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from models import Category
 from schemas import CategoryModel
 from database import Session, engine
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import selectinload
 
 
 
@@ -12,17 +13,17 @@ category_router=APIRouter(
 )
 session=Session(bind=engine)
 
-
-
 @category_router.get('/')
 async def list_all_items():
       items=session.query(Category).all()
       return jsonable_encoder(items)
 
 @category_router.get('/{id}')
-async def get_item_by_id(id:int):
-     item=session.query(Category).filter(Category.id==id).first()
-     return jsonable_encoder(item)
+async def get_item_by_id(id: int):
+    item = session.query(Category).options(selectinload(Category.cat_pizza)).filter(Category.id == id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return item
 
 
 @category_router.post('/', status_code=status.HTTP_201_CREATED)
